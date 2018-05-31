ca1f88c514bb33ecce784b3290c4a23d3d6abb6a
Akram Hossain Rickon <akramhsin29@ggmai.com>
2018-06-01 01:07:45 +0600

    Binary search and maxmin file added

---
+++ Binary search.cpp
@@ -0,0 +1,35 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int a[], int low, int high, int x)
+{
+    if (low<=high)
+    {
+        int mid = (low+high)/2;
+        if (a[mid] == x)     return mid;
+
+        if (a[mid] > x)  return binarySearch(a, low, mid-1, x);
+        else             return binarySearch(a, mid+1,high, x);
+    }
+    return -1;
+}
+
+int main()
+{
+    int a[100],n,x,i,low,high;
+    cin>>n;
+
+    for(i=0 ; i<n ; i++) cin>>a[i];
+
+    cin>>x;
+
+    low=0,high=n-1;
+    int result = binarySearch(a,low,high,x);
+
+    if(result == -1) cout<<"Item is not present in array"<<endl;
+    else             cout<<"Item is present at position:"<<result+1<<endl;
+    return 0;
+}
+
